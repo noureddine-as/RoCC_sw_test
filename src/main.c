144d1de8d5cc7b6d777d633519fba5f40f60efd2
noureddine-as <noureddine.asni@gmail.com>
2018-06-29 14:36:10 +0200

    Back to Seldridge original example

--- src/main.c
+++ src/main.c
@@ -48,38 +48,36 @@ int main() {
 
   unsigned long int data [] = {0xdead, 0xbeef, 0x0bad, 0xf00d}, y;
 
-  // unsigned short addr = 1;
-  // printf("[INFO] Write R[%d] = 0x%lx\n", addr, data[0]);
-  // doWrite(y, addr, data[0]);
-  unsigned short addr = 2;
+  unsigned short addr = 1;
+  printf("[INFO] Write R[%d] = 0x%lx\n", addr, data[0]);
   doWrite(y, addr, data[0]);
 
-  // printf("[INFO] Read R[%d]\n", addr);
-  // doRead(y, addr);
-  // printf("[INFO]   Received 0x%lx (expected 0x%lx)\n", y, data[0]);
-  // assert(y == data[0]);
+  printf("[INFO] Read R[%d]\n", addr);
+  doRead(y, addr);
+  printf("[INFO]   Received 0x%lx (expected 0x%lx)\n", y, data[0]);
+  assert(y == data[0]);
 
-  // unsigned long int data_accum = -data[0] + data[1];
-  // printf("[INFO] Accum R[%d] with 0x%lx\n", addr, data_accum);
-  // doAccum(y, addr, data_accum);
-  // assert(y == data[0]);
+  unsigned long int data_accum = -data[0] + data[1];
+  printf("[INFO] Accum R[%d] with 0x%lx\n", addr, data_accum);
+  doAccum(y, addr, data_accum);
+  assert(y == data[0]);
 
-  // printf("[INFO] Read R[%d]\n", addr);
-  // doRead(y, addr);
-  // printf("[INFO]   Received 0x%lx (expected 0x%lx)\n", y, data[1]);
-  // assert(y == data[1]);
+  printf("[INFO] Read R[%d]\n", addr);
+  doRead(y, addr);
+  printf("[INFO]   Received 0x%lx (expected 0x%lx)\n", y, data[1]);
+  assert(y == data[1]);
 
-  // unsigned long int data_addr;
-  // doTranslate(data_addr, &data[2]);
-  // printf("[INFO] Load 0x%lx (virt: 0x%p, phys: 0x%p) via L1 virtual address\n",
-  //        data[2], &data[2], (void *) data_addr);
-  // doLoad(y, addr, &data[2]);
-  // assert(y == data[1]);
+  unsigned long int data_addr;
+  doTranslate(data_addr, &data[2]);
+  printf("[INFO] Load 0x%lx (virt: 0x%p, phys: 0x%p) via L1 virtual address\n",
+         data[2], &data[2], (void *) data_addr);
+  doLoad(y, addr, &data[2]);
+  assert(y == data[1]);
 
-  // printf("[INFO] Read R[%d]\n", addr);
-  // doRead(y, addr);
-  // printf("[INFO]   Received 0x%lx (expected 0x%lx)\n", y, data[2]);
-  // assert(y == data[2]);
+  printf("[INFO] Read R[%d]\n", addr);
+  doRead(y, addr);
+  printf("[INFO]   Received 0x%lx (expected 0x%lx)\n", y, data[2]);
+  assert(y == data[2]);
 
 
 
